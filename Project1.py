584d06717e469f0f213a1e15cfdf8ed1ac212f45
Deekshitha Prabhakar <deeksdp03@gmail.com>
2023-07-05 08:18:26 +0530

    Operations of 2 numbers

---
+++ Project1.py
@@ -0,0 +1,24 @@
+#operations of 2 numbers
+def addition():
+  a=int(input("Enter a number"))
+  b=int(input("Enter another number"))
+  c= a+b
+  print(f"the sum is {c}")
+  return c
+addition()
+
+def subtraction():
+  a=int(input("Enter a number"))
+  b=int(input("Enter another number"))
+  c= a-b
+  print(f"the difference is {c}")
+  return c
+subtraction()
+
+def multiplication():
+  a=int(input("Enter a number"))
+  b=int(input("Enter another number"))
+  c= a*b
+  print(f"the product is {c}")
+  return c
+multiplication()
